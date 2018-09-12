6e3e68b2d3e1c8638dc4eac8529df239dc24ce2f
wolfwm <wolfwalder@gmail.com>
2018-09-12 18:35:36 -0300

    introduzido supporte para chefes/ introduzido diferencas de atributos para personagens m e f/ feita estrutura de estoria

--- PyRPG.py
+++ PyRPG.py
@@ -6,6 +6,8 @@ monlist3 = []
 monlist4 = []
 monlist5 = []
 
+bosslist = []
+
 itemlist = []
 
 magiclist = []
@@ -13,20 +15,22 @@ magiclist = []
 class Monster:
 
     def __init__(self, name, hp, atk, dif):
-        if(0<dif<=5):
+        if(0<dif<=6):
             self.name = name
             self.hp = hp
             self.atk = atk
-            if(dif<=1):
+            if(dif==1):
                 monlist1.append(self)
-            elif(dif<=2):
+            elif(dif==2):
                 monlist2.append(self)
-            elif(dif<=3):
+            elif(dif==3):
                 monlist3.append(self)
-            elif(dif<=4):
+            elif(dif==4):
                 monlist4.append(self)
-            else:
+            elif(dif==5):
                 monlist5.append(self)
+            else:
+                bosslist.append(self)
         else:
             print('ERRO: Dificuldade de monstro inválida (', name,')')
 
@@ -36,17 +40,28 @@ lobo = Monster('Lobo',100,20,1)
 loboF = Monster('Lobo Faminto',130,23,2)
 loboA = Monster('Lobo Alpha',150,26,3)
 loboH = Monster('Lobisomem',160,28,4)
-loboM = Monster('Lobo Mau',300,30,5)
+loboM = Monster('Lobo Mau',180,30,5)
 
 # fim da lista de monstros
+
+# inicio da lista de chefes ( dif = 6 )
+
+loboMP = Monster('Lobo Mau Picapau',200,33,6)
+
+# fim da lista de chefes
 
 class Player:
     def __init__(self,sex,name):
         self.sex = sex
         self.name = name
-        self.hp = 400
-        self.atk = 40
-        self.mp = 200
+        if(sex=='m'):
+            self.hp = 410
+            self.atk = 45
+            self.mp = 150
+        else:
+            self.hp = 400
+            self.atk = 40
+            self.mp = 200
         self.money = 20
         self.runCount = 0
         self.deathCount = 0
@@ -82,10 +97,13 @@ def battle(level):
         mon = monlist4[random.randint(0, len(monlist4) - 1)]
     elif(level==5):
         mon = monlist5[random.randint(0, len(monlist5) - 1)]
+    elif(level==6):
+        mon = bosslist[random.randint(0, len(bosslist) - 1)]
+
+        # dificuldade 6 é uma luta de chefe
+
     else:
         print('ERRO: Dificuldade de batalha inválida ( nível',level,')')
-
-    print('\nUm',mon.name,'aparece!')
 
     run=False
     enemyHP = mon.hp
@@ -103,6 +121,7 @@ def battle(level):
                 run=True
                 player.runCount += 1
                 print(player.name,'fugiu de', mon.name,'\n')
+                return 3
             else:
                 print('Escolha inválida (',plyrinpt,')')
         if(enemyHP > 0 and player.hp > 0 and run==False):
@@ -111,15 +130,64 @@ def battle(level):
         if(enemyHP<=0):
             player.atk += 1
             print('\nVitória!',player.name,'se sente mais forte\n')
+            return 1
         if(player.hp<=0):
             player.deathCount += 1
             player.hp=400
             run=True
             print('\nDerrota...')
-            print(player.name,'acordou horas depois no local da batalha\n')
+            return 2
 
-battle(1)
-battle(2)
-battle(3)
-battle(4)
-battle(5)
+storyIndex = 0
+if(battle(1)==1):
+    print('Estoria se vitória ( batalha 1 )')
+    storyIndex=1
+else:
+    print('Estória se derrota ou fuga ( batalha 1 )')
+    storyIndex=2
+
+if(storyIndex==1):
+    storyIndex=battle(2)
+    if(storyIndex==1):
+        print('Estoria se vitória ( batalha 2 )')
+        storyIndex=1
+    elif(storyIndex==2):
+        print('Estória se derrota ( batalha 2 )')
+        storyIndex=2
+    else:
+        print('Estória se fuga ( batalha 2 )')
+        storyIndex=3
+else:
+    test=False
+    while(test==False):
+        print('Escolha entre as opções\nX ( 1 )\nY ( 2 )\nZ ( 3 )')
+        storyIndex=int(input('Seleção: '))
+        if(storyIndex==1):
+            print('Consequência de X')
+            test=True
+            storyIndex=2
+        elif(storyIndex==2):
+            print('Consequência de Y')
+            test=True
+        elif(storyIndex==3):
+            print('Consequência de Z')
+            test=True
+
+        else:
+            print('Escolha inválida (',storyIndex,')')
+
+if(storyIndex==1):
+    print('Estoria pacifica')
+elif(storyIndex==2):
+    storyIndex = battle(6)
+    if (storyIndex == 1):
+        print('Estoria se vitória ( batalha 6 )')
+        storyIndex = 1
+    elif (storyIndex == 2):
+        print('Estória se derrota ( batalha 6 )')
+        storyIndex = 2
+    else:
+        print('Estória se fuga ( batalha 6 )')
+        storyIndex = 3
+else:
+    print('Estoria com desavenças')
